34d61ed0dc12702e6c5e201623f93f3416a5b584
justMSARahat <justshamsulalom@gmail.com>
2025-04-02 08:50:57 +0600

    Frontend Done

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("Logo").addEventListener('click', function(){
+    window.location.href ="index.html";
+});
+
+document.getElementById("BlogPage").addEventListener('click', function(){
+    window.location.href ="blog.html";
+});
+
+document.getElementById("BackToHome").addEventListener('click', function(){
+    window.location.href ="index.html";
+});
